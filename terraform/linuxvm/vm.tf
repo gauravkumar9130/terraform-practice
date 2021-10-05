ec2c909dc965b1e8e00b08f2ef5a80c8abe84afd
abc <ga@gmail.com>
2021-10-05 18:01:53 +0530

    terraform

--- terraform/linuxvm/vm.tf
+++ terraform/linuxvm/vm.tf
@@ -3,34 +3,52 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "myrg"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "myvnet"
+  name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "internal"
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "example" {
-  count = 5
+  count = var.number_of_vms
   name                = "pip-${count.index}"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
 }
 
+resource "azurerm_network_security_group" "example" {
+  name                = var.network_security_group_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_network_interface" "example" {
-  count = 5
+  count = var.number_of_vms
   name                = "nic-${count.index}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -43,19 +61,31 @@ resource "azurerm_network_interface" "example" {
   }
 }
 
+resource "azurerm_network_interface_security_group_association" "association" {
+  count = var.number_of_vms
+  network_interface_id      = azurerm_network_interface.example.*.id[count.index]
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "azurerm_linux_virtual_machine" "example" {
-  count = 5
+  count = var.number_of_vms
   name                = "vm-${count.index}"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password = "Pa$$w0rd123"
-  disable_password_authentication = "false"
+  admin_username      = var.username
   network_interface_ids = [
     azurerm_network_interface.example.*.id[count.index],
   ]
 
+  admin_ssh_key {
+    username   = var.username
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
 
   os_disk {
     caching              = "ReadWrite"
@@ -70,6 +100,11 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 
   provisioner "local-exec" {
-    command = "echo ${azurerm_public_ip.example.*.ip_address[count.index]} >> /home/ansible/inventory"
+    command = "echo ${azurerm_public_ip.example.*.ip_address[count.index]} >> inventory"
   }
+}
+
+resource "local_file" "mypem" {
+    filename = "mykey.pem"
+    content = tls_private_key.example_ssh.private_key_pem
 }
